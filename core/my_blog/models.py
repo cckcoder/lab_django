a130cd024792c2a0743c34274ecd1d736a495211
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-02-06 22:41:00 +0700

    update: mv app inside project folder

--- core/my_blog/models.py
+++ core/my_blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class Blog(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=120)
     post = models.TextField()
     date_created = models.DateTimeField()
     date_updated = models.DateTimeField()
 
+    def __str__(self):
+        return self.title
