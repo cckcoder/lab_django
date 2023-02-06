a2a415895cf113208e713089d6d9e56ef6d80e02
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-02-06 22:27:12 +0700

    create: blog models

--- my_blog/models.py
+++ my_blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    post = models.TextField()
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+
