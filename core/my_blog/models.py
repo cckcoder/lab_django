52228946e3d4e96a4871731f0ef367401b2344d4
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-02-07 08:03:48 +0700

    add: autonow to date field

--- core/my_blog/models.py
+++ core/my_blog/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=120)
     post = models.TextField()
-    date_created = models.DateTimeField()
-    date_updated = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
